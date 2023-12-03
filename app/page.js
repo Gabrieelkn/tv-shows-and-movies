d649d96fe23baf82c89e0f87b0271c5af644f80f
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2023-12-03 14:09:09 +0200

    ok

--- app/page.js
+++ app/page.js
@@ -1,16 +1,22 @@
 "use client";
+import { useState, useEffect } from "react";
 import { useShows } from "@/context/MovieProvider";
 export default function Home() {
   const { history } = useShows();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   return (
-    <div>
-      <h1>Search for a movie or a tv show</h1>
-      <p>Your search history:</p>
+    isClient && (
       <div>
-        {history.map((item, index) => (
-          <p key={index}>{item}</p>
-        ))}
+        <h1>Search for a movie or a tv show</h1>
+        <p>Your search history:</p>
+        <div>
+          {history && history.map((item, index) => <p key={index}>{item}</p>)}
+        </div>
       </div>
-    </div>
+    )
   );
 }
